5f307cfca27cd15ef0a6c298aa4456129573a4dc
Shuber Ali Mirza <72148880+V3nUM3440@users.noreply.github.com>
2022-08-13 12:47:15 +0400

    Add files via upload

---
+++ Graph/graph_harness.py
@@ -0,0 +1,211 @@
+# 
+# Author ----------- Shuber Ali Mirza
+# graph_harness.py - Test harness for graphs
+# Created ---------- 21/FEB/2021
+# Last Updated ----- 22/FEB/2021
+# 
+
+from graphs import *
+import pickle, sys, os
+
+print('\n# ------------------------- Graphs Harness ------------------------- #')
+
+print('''
+Type:
+D\tFor directed
+U\tFor undirected
+''')
+
+direct = None
+user = None
+
+while direct == None:
+    direct = input('Enter option: ').lower()
+    if direct == 'd':    
+        user = DSAGraph(True)
+    elif direct == 'u':
+        user = DSAGraph(False)
+    else:
+        direct = None
+        print('ERROR - Unknown option')
+
+print('''
+Type:
+AV\tTo add vertex
+AE\tTo add edge
+HV\tTo check is graph has vertex
+VC\tTo get vertex count
+EC\tTo get edge count
+GV\tTo get a vertex
+GA\tTo get adjacency list
+IA\tTo check if vertices are adjacent
+DL\tTo display as list
+DM\tTo display as Matrix
+BFS\tTo perform breadth first search
+PK\tTo manipulate graph using pickle
+FILE\tTo read and write to a file
+Q\tTo quit test harness
+''')
+
+option = None
+
+while option != 'q':
+    option = input('\nEnter option: ').lower()
+    if option == 'av':
+        l = input('Enter label: ')
+        v = input('Enter value: ')
+        user.addVertex(l, v)
+        
+    elif option == 'ae':
+        try:
+            l1 = input('Enter label 1: ')
+            l2 = input('Enter label 2: ')
+            if user.hasVertex(l1) == False:
+                raise Exception
+            elif user.hasVertex(l2) == False:
+                raise Exception
+            else:
+                user.addEdge(l1, l2)
+        except Exception:
+            print('ERROR - label doesn\'t exist')
+        
+    elif option == 'hv':
+        l = input('Enter label: ')
+        print(user.hasVertex(l))
+            
+    elif option == 'vc':
+        print(user.vertexCount())
+    
+    elif option == 'ec':
+        print(user.edgeCount())
+        
+    elif option == 'gv':
+        l = input('Enter label: ')
+        print(user.getVertex(l))
+        
+    elif option == 'ga':
+        l = input('Enter label: ')
+        user.getAdjacent(l)
+    
+    elif option == 'ia':
+        try:
+            l1 = input('Enter label 1: ')
+            l2 = input('Enter label 2: ')
+            if user.hasVertex(l1) == False:
+                raise Exception
+            elif user.hasVertex(l2) == False:
+                raise Exception
+            else:
+                print(user.isAdjacent(l1, l2))
+        except Exception:
+            print('ERROR - label doesn\'t exist')
+        
+    elif option == 'dl':
+        user.displayList()
+    
+    elif option == 'dm':
+        user.displayMatrix()
+        
+    elif option == 'bfs':
+        user.breadthFirst()
+            
+    elif option == 'pk':
+        print('''
+Type:
+W\tTo write your current graph to serialized file
+P\tTo print the graph present in the file in as adjacency list
+R\tTo read the serialized file
+Q\tTo quit pickling
+        ''')
+        optionp = None
+        
+        while optionp != 'q':
+            optionp = input('\nEnter an option (pickle menu): ').lower()
+            if optionp == 'q':
+                break
+            elif optionp == 'w':
+                try:
+                    with open('pickleGraph.txt' , 'wb') as sgraph:
+                        pickle.dump(user , sgraph)
+                        print('Graph serialized to pickleGraph.txt')
+                except:
+                    print('ERROR - Problem pickling object')
+            elif optionp == 'r':
+                try:
+                    with open('pickleGraph.txt' , 'rb') as sgraph:
+                        user = pickle.load(sgraph)
+                except:
+                    print('ERROR - File does not exist')
+                    
+            elif optionp == 'p':
+                try:
+                    with open('pickleGraph.txt' , 'rb') as sgraph:
+                        read = pickle.load(sgraph)
+                        read.displayList()
+                except:
+                    print('ERROR - File does not exist')
+            else:
+                print('ERROR - Unknown option')
+    
+    elif option == 'file':
+        print('''
+Type:
+W\tTo write a file
+P\tTo print content of a file
+R\tTo read a file
+Q\tTo quit csv menu
+        ''')
+        
+        optionc = None
+        
+        while optionc != 'q':
+            optionc = input('\nEnter an option (file menu): ').lower()
+            
+            if optionc == 'q':
+                break
+            elif optionc == 'r':
+                try:
+                    f = input('Enter file name (with extension): ')
+                    with open(f, 'r') as g:
+                        stdout_ = sys.stdout
+                        sys.stdout = open('trash.txt', 'w')
+                        for line in g:
+                            l = line.split(' ')
+                            l1 = l[0]
+                            l2 = l[1].strip()
+                            user.addVertex(l1 , ' ')
+                            user.addVertex(l2, ' ')
+                            user.addEdge(l1, l2)
+                        sys.stdout = sys.__stdout__
+                        os.remove('trash.txt')
+                except:
+                    print('ERROR - File does not exist')
+                
+            elif optionc == 'w':
+                f = input('Enter file name (with extension): ')
+                stdout_ = sys.stdout
+                sys.stdout = open(f, 'w')
+                for i in user.vertices:
+                    for link in i.links:
+                        if link != None:
+                            print(i.label, link.getValue())
+                sys.stdout = sys.__stdout__
+                    
+            elif optionc == 'p':
+                f = input('Enter file name (with extension): ')
+                try:
+                    with open(f , 'r') as cf:
+                        for line in cf:
+                            print(line)
+                except:
+                    print('ERROR - File does not exist')
+                
+            else:
+                print('ERROR - Unknown option')
+        
+    
+    elif option == 'q':
+        break
+        
+    else:
+        print('ERROR - Unknown option')
---
+++ Graph/graphs.py
@@ -0,0 +1,229 @@
+#
+# Author ------- Shuber Ali Mirza
+# graphs.py ---- Contains class for graphs
+# Created ------ 18/FEB/2021
+# Last Updated - 22/FEB/2021
+#
+
+from linked_list import *
+from queue import *
+import numpy as np
+
+class EmptyError(RuntimeError):
+    def __init__(self, arg):
+        self.args = arg
+
+
+class DSAGraphVertex(DSAListNode):
+    def __init__(self, label, value):
+        self.label = label
+        self.value = value
+        self.links = DSALinkedList()
+        self.visited = None
+            
+    def addLink(self, vertex):
+        self.links.insertLast(DSAListNode(vertex.label))
+        self.links.peekLast().setNext(vertex)
+    
+    def getLinks(self):
+#         print('\nLinks of: ', self.label)
+        for i in self.links:
+            if i == None:
+                print('No links')
+            else:
+                print(i.getValue())
+    
+    def __str__(self):
+        return ('Label: ' + str(self.label) + ' Value: ' + str(self.value))
+
+
+class DSAGraph(DSAGraphVertex):
+    
+    def __init__(self, directed):
+        self.vertices = DSALinkedList()
+        self.directed = directed
+    
+    def isEmpty(self):
+        empty = False
+        for i in self.vertices:
+            if i == None:
+                empty = True
+        return empty
+        
+    def addVertex(self, label, value):
+        try:
+            for i in self.vertices:
+                if i != None:
+                    if i.label == label:
+                        raise Exception
+            self.vertices.insertLast(DSAGraphVertex(label, value))
+        except Exception:
+            print('ERROR - Label already exists')
+        
+    def addEdge(self, label1, label2):
+        try:
+            if label1 == label2:
+                raise ValueError
+            elif self.isEmpty():
+                raise EmptyError('')
+            elif self.isAdjacent(label1, label2):
+                raise Exception
+            else:
+                for i in self.vertices:
+                    if i.label == label1:
+                        for j in self.vertices:
+                            if j.label == label2:
+                                if self.directed == False:
+                                    i.addLink(j)
+                                    j.addLink(i)
+                                else:
+                                    i.addLink(j)
+        except ValueError:
+            print('ERROR - Edge cannot be added to same vertex')
+        except EmptyError:
+            print('ERROR - Graph empty')
+        except Exception:
+            print('ERROR - Edge already exists')
+        
+    
+    def hasVertex(self, label):
+        present = False
+        try:
+            if self.isEmpty():
+                raise EmptyError('')
+            else:
+                for i in self.vertices:
+                    if i.label == label:
+                        present = True
+        except EmptyError:
+            print('ERROR - Graph empty')
+        return present
+    
+    def vertexCount(self):
+        count = 0
+        try:
+            if self.isEmpty():
+                raise EmptyError('')
+            else:
+                for i in self.vertices:
+                    count += 1
+        except EmptyError:
+            print('ERROR - Graph empty')
+        return count
+    
+    def edgeCount(self):
+        count = 0
+        try:
+            if self.isEmpty():
+                raise EmptyError('')
+            else:
+                for i in self.vertices:
+                    for link in i.links:
+                        if link != None:
+                            count += 1
+                if self.directed == False:
+                    count = count / 2
+        except EmptyError:
+            print('ERROR - Graph Empty')
+        return int(count)
+    
+    def getVertex(self, label):
+        v = None
+        if self.hasVertex(label) == True:
+            for vertex in self.vertices:
+                if vertex.label == label:
+                    v = vertex
+        return v
+    
+    def getAdjacent(self, label):
+        try:
+            if self.isEmpty():
+                raise EmptyError('')
+            else:
+                for vertex in self.vertices:
+                    if vertex.label == label:
+                        vertex.getLinks()
+        except EmptyError:
+            print('ERROR - Graph empty')
+                
+    def isAdjacent(self, label1, label2):
+        linked = False
+        try:
+            if self.isEmpty():
+                raise EmptyError('')
+            else:
+                for i in self.vertices:
+                    if i.label == label1:
+                        for j in self.vertices:
+                            if j.label == label2:
+                                for linki in i.links:
+                                    for linkj in j.links:
+                                        if linki != None and linki.getValue() == label2:
+                                            linked = True
+                                        elif linkj != None and linkj.getValue() == label1:
+                                            linked = True
+        except EmptyError:
+            print('ERROR - Graph empty')
+        return linked
+
+    def displayList(self):
+        try:
+            if self.isEmpty():
+                raise EmptyError('')
+            else:
+                for i in self.vertices:
+                    string = i.label + '|'
+                    for link in i.links:
+                        if link != None:
+                            string += ' ' + link.getValue()
+                    print(string)
+        except EmptyError:
+            print('ERROR - Graph Empty')
+
+    def displayMatrix(self):
+        try:
+            if self.isEmpty():
+                raise EmptyError('')
+            else:
+                count = int(self.vertexCount())
+                m = np.zeros((count+1, count+1), dtype=np.str)
+                i = 1
+                l = 1
+                for e in range(count+1):
+                    for el in range(count+1):
+                        m[e][el] = 0
+                for j in self.vertices:
+                    m[0][i] = j.label
+                    i += 1
+                for k in self.vertices:
+                    m[l][0] = k.label
+                    l += 1
+
+                for r in range(count+1):
+                    for c in range(count+1):
+                        if self.isAdjacent(m[r][0], m[0][c]):
+                            for link in self.getVertex(m[r][0]).links:
+                                if link != None and link.getValue() == m[0][c]:
+                                    m[r][c] = '1'
+                m[0][0] = ' '
+                print(m)
+        except EmptyError:
+            print('ERROR - Graph empty')
+            
+    def breadthFirst(self):
+        try:
+            if self.isEmpty():
+                raise EmptyError('')
+            q = DSAQueue()
+            BFS = ''
+            for i in self.vertices:
+                BFS += i.label
+                q.enqueue(i.label)
+                for link in i.links:
+                    if link != None:
+                        q.enqueue(link.getValue())
+                q.dequeue()
+            print(BFS)
+            
+        except EmptyError:
+            print('ERROR - Graph empty')
